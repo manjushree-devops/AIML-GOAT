5842d221798ddc78db0eea156bf8c57a5d3f0369
Keerthan GK <keerthangk.aiml@skit.org.in>
2026-02-18 21:18:49 +0530

    Added Python code for the dataset Disease_diagnosis

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+import pandas as pd
+
+app = Flask(__name__)
+
+# Load dataset
+data = pd.read_csv("disease_diagnosis.csv")
+
+@app.route("/")
+def home():
+    # Convert dataframe to HTML table
+    table = data.head(20).to_html(classes='data', header="true", index=False)
+    return render_template("index.html", table=table)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
